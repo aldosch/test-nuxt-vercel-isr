21f73bb357bb68b7494603e79eb71f53de932a95
aldosch <git@aldo.io>
2023-12-14 14:57:18 +1100

    add nitro config for on-demand isr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,27 @@
 export default defineNuxtConfig({
+  nitro: {
+    vercel: {
+      config: {
+        bypassToken: process.env.VERCEL_BYPASS_TOKEN,
+      },
+    },
+  },
   routeRules: {
     // revalidated every 60 seconds, in the background
-    '/**': { isr: 60 },
+    "/**": { isr: 60 },
     // this page will be always fresh
-    '/dynamic': { isr: false },
+    "/dynamic": { isr: false },
     // this page will be generated on demand and then cached permanently
-    '/static': { isr: true },
+    "/static": { isr: true },
     // this page is generated at build time and cached permanently
-    '/prerendered': { prerender: true },
+    "/prerendered": { prerender: true },
     // you can do lots more with route rules too!
-    '/redirect': { redirect: '/static' },
-    '/headers': { headers: { 'x-magic-of': 'nuxt and vercel' } },
-    '/spa': { ssr: false },
+    "/redirect": { redirect: "/static" },
+    "/headers": { headers: { "x-magic-of": "nuxt and vercel" } },
+    "/spa": { ssr: false },
   },
   runtimeConfig: {
-    nitro: { envPrefix: 'VERCEL_' },
+    nitro: { envPrefix: "VERCEL_" },
     region: process.env.VERCEL_REGION,
   },
-})
+});
